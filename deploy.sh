08b250fbee67b68b149eb0130059ff5125e4d1ad
mastercoks <matheusbdo1@gmail.com>
2022-09-15 23:24:03 -0300

    💥 add https config

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,19 @@ EMAIL=${EMAIL}
 DOMAIN=${DOMAIN-localhost}
 USER=${USER-admin}
 PASS=${PASS-admin}
+PROTOCOL=${PROTOCOL-http}
 
 printf "${USER}:$(openssl passwd -apr1 ${PASS})\n" > ./htpasswd
 
-echo "deploying traefik stack in http mode"
-docker stack deploy -c docker-compose.yml infra
-echo "Traefik UI is available."
+if [ ${PROTOCOL} == "http" ] 
+  then
+    echo "deploying traefik stack in http mode"
+    docker stack deploy -c infra_http.yml infra
+    echo "Traefik UI is available at:"
+    echo "- http://${DOMAIN}"
+  else
+    echo "deploying traefik stack in https mode"
+    docker stack deploy -c infra_https.yml infra
+    echo "Traefik UI is available at:"
+    echo "- https://${DOMAIN}"
+fi
